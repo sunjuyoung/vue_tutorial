fccdb9375829162e6f0e3669e7653bfc1e814b72
Sun <syseoz@naver.com>
2020-11-11 22:35:55 +0900

    vuetify tutorial

---
+++ ex07-03/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <!-- 윈쪽 메뉴아이콘 넣음 -->
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>멀티페이지</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+    </v-app-bar>
+
+   
+    <v-content>
+       <!-- 페이지 장면 전환 효과 넣기 -->
+      <v-slide-x-transition mode="out-in">
+        <router-view></router-view>
+      </v-slide-x-transition>
+    </v-content>
+
+    <v-footer color="secondary" fixed dark>
+      <div class="mx-auto">CODE-DESIGN.web.app</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+</script>
---
+++ ex07-03/src/components/main_page.vue
@@ -0,0 +1,13 @@
+<template>
+        <v-container>
+            <p class="display-1 my-4">메인페이지입니다.</p>
+            <v-divider></v-divider>
+            <p class="display-4 my-4">메인 페이지입니다.</p>
+            <div class="text-content">
+                <!-- 버튼에서 fab large 사용하여 큰원으로 설정 -->
+                <v-btn fab large class="mt-5" color="purple" dark to="/sub">
+                <v-icon>mdi-arrow-right</v-icon>
+                </v-btn>
+            </div>
+        </v-container>
+</template>
