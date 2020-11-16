b8878ccf9a9b7266fd698dd77caf77cbafc90233
Sun <syseoz@naver.com>
2020-11-16 21:42:24 +0900

    firebase 연동

---
+++ ex08_start/dist/precache-manifest.b0b4277fbfeef184bf73b7547273c38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603767ab64a4216570bf",
+    "url": "/css/chunk-vendors.f9baab0f.css"
+  },
+  {
+    "revision": "ad375be1353eb0f5378205f3c932d6bc",
+    "url": "/img/hello-pwa.ad375be1.png"
+  },
+  {
+    "revision": "785bdc176a6f9d62274f949db0a425a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab982afac8a261988e7",
+    "url": "/js/app.456d3771.js"
+  },
+  {
+    "revision": "603767ab64a4216570bf",
+    "url": "/js/chunk-vendors.9ffe6bf5.js"
+  },
+  {
+    "revision": "749f151ab801440784fcb2b9dc6e3d0a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ ex08_start/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-content>
+      <!-- fill-height는 브라우저 높이를 100%, 수직으로 가운데 정렬 -->
+      <v-container fluid fill-height><!-- fluid 좌우 여백을 제거 -->
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="title white--text" >반가와요</h1>
+            <p class="caption">by VueJs</p>
+            <img src="./assets/hello-pwa.png" alt="">
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  created(){
+    //배경색을 다크모드로 함
+    this.$vuetify.theme.dark=true;
+  }
+
+};
+</script>
